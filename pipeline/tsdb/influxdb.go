b55f1132b3c23120d95896bf59fbb2daee9e4bf4
David Claudio Gonzalez <d.claudiogonzalez@gmail.com>
2023-03-07 15:51:23 -0600

    Convert tsdb to a package
    Signed-off-by: David Claudio Gonzalez <d.claudiogonzalez@gmail.com>

--- pipeline/tsdb/influxdb.go
+++ pipeline/tsdb/influxdb.go
@@ -1,4 +1,4 @@
-package main
+package tsdb
 
 import (
 	"context"
--- pipeline/tsdb/run.go
+++ pipeline/tsdb/run.go
@@ -1,9 +1,13 @@
 package main
 
+import (
+    "github.com/forzaclaudio/driving-data-engineering/pipeline/tsbd"
+)
+
 func main(){
-    myTSDB := &TSDB{}
+    myTSDB := &influxdb.TSDB{}
     myTSDB.Initialize()
-    mySession := &SimSession{}
+    mySession := &influxdb.SimSession{}
     mySession.Initialize()
     myTSDB.SaveDataPointWithUnits(mySession, "km/h", "speed", 180.0)
     myTSDB.SaveDataPoint(mySession, "throttle", 100.0)
