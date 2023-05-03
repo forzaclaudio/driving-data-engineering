f16ceef12c1121ff5185725722d55c57a305dee9
David Claudio Gonzalez <d.claudiogonzalez@gmail.com>
2023-05-03 08:29:57 -0500

    Refactor tsdb package
    Signed-off-by: David Claudio Gonzalez <d.claudiogonzalez@gmail.com>

--- pkg/tsdb/influxdb.go
+++ pkg/tsdb/influxdb.go
@@ -2,120 +2,161 @@ package influxdb
 
 import (
 	"context"
-	"log"
+	"errors"
 	"fmt"
+	"log"
 	"path/filepath"
 	"time"
 
+	influxdb2 "github.com/influxdata/influxdb-client-go/v2"
 	toml "github.com/pelletier/go-toml"
-	"github.com/influxdata/influxdb-client-go/v2"
 )
 
 type TSDB struct {
-    host string
-    port int64
-    token string
-    uri string
-    org string
-    bucket string
-
+	host   string
+	port   int64
+	token  string
+	uri    string
+	org    string
+	bucket string
 }
 
-func handleFatalError(msg string, err error){
-  if err != nil {
-    log.Fatal(msg, err.Error())
-  }
-}
-
-func handleNonFatalError(msg string, err error){
+func handleNonFatalError(msg string, err error) {
 	if err != nil {
 		log.Println(err.Error())
 	}
 }
 
-
 type SimSession struct {
-    location string
-    driver string
-    gender string
+	location string
+	driver   string
+	gender   string
 }
 
-func (tsdb *TSDB) Initialize(configPath string) {
-    log.Println("Initializing TSDB...")
-    config_path, err := filepath.Abs(configPath)
-    handleFatalError("When generating config_path", err)
-    config, err := toml.LoadFile(config_path)
-    handleFatalError("When reading config file", err)
-    tsdb.host = config.Get("influxdb_connection.host").(string)
-    tsdb.port = config.Get("influxdb_connection.port").(int64)
-    tsdb.uri = fmt.Sprintf("http://%s:%d", tsdb.host, tsdb.port)
-    tsdb.token = config.Get("influxdb_connection.token").(string)
-    tsdb.org = config.Get("influxdb_connection.org").(string)
-    tsdb.bucket = config.Get("influxdb_connection.bucket").(string)
-    log.Println(tsdb.uri, tsdb.org, tsdb.bucket)
+func (tsdb *TSDB) ParseValues(config *toml.Tree) (err error) {
+	defer func() error {
+		if recover() != nil {
+			err = errors.New("[TSDB Initialize]: failed to parse one or various required values.")
+		}
+		return err
+	}()
+	tsdb.host = config.Get("influxdb_connection.host").(string)
+	tsdb.port = config.Get("influxdb_connection.port").(int64)
+	tsdb.uri = fmt.Sprintf("http://%s:%d", tsdb.host, tsdb.port)
+	tsdb.token = config.Get("influxdb_connection.token").(string)
+	tsdb.org = config.Get("influxdb_connection.org").(string)
+	tsdb.bucket = config.Get("influxdb_connection.bucket").(string)
+	log.Println("Connection initialized as:", tsdb.uri, tsdb.org, tsdb.bucket)
+	return nil
 }
 
-func (ss *SimSession) Initialize(configPath string) {
-    log.Println("Initializing SimSession...")
-    session_path, err := filepath.Abs(configPath)
-    if err != nil{
-        log.Fatal(err)
-    }
-    config, err := toml.LoadFile(session_path)
-    ss.location = config.Get("session.location").(string)
-
-    ss.driver = config.Get("session.driver").(string)
-    ss.gender = config.Get("session.gender").(string)
-    log.Println("Session initialized as:", ss)
+func (tsdb *TSDB) Initialize(configPath string) error {
+	log.Println("Initializing TSDB...")
+	config_path, err := filepath.Abs(configPath)
+	if err != nil {
+		return fmt.Errorf("[TSDB Initialize]: failed to generate configPath: %w", err)
+	}
+	config, err := toml.LoadFile(config_path)
+	if err != nil {
+		return fmt.Errorf("[TSDB Initialize]: failed to load config file: %w", err)
+	}
+	err = tsdb.ParseValues(config)
+	if err != nil {
+		return fmt.Errorf("[TSDB Initialize]: failed to parse values: %w", err)
+	}
+	return nil
 }
 
-func (tsdb *TSDB) SaveIntDataPoint(ss *SimSession, fieldName string, value int32) {
-    influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
-    writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
-    q := influxdb2.NewPointWithMeasurement(ss.driver).
-            AddTag("location", ss.location).
-            AddField(fieldName, value).
-            SetTime(time.Now())
-    err := writeAPI.WritePoint(context.Background(), q)
-    handleNonFatalError("When saving integer datapoint", err)
-    influx_client.Close()
+func (ss *SimSession) ParseValues(config *toml.Tree) (err error) {
+	defer func() error {
+		if recover() != nil {
+			err = errors.New("[SimSession Initialize]: failed to parse one or various required values.")
+		}
+		return err
+	}()
+	ss.location = config.Get("session.location").(string)
+	ss.driver = config.Get("session.driver").(string)
+	ss.gender = config.Get("session.gender").(string)
+	log.Println("Session initialized as:", ss)
+	return nil
 }
 
-func (tsdb *TSDB) SaveIntDataPointWithUnits(ss *SimSession, units string, fieldName string, value int32) {
-        influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
-        writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
-        p := influxdb2.NewPointWithMeasurement(ss.driver).
-                AddTag("location", ss.location).
-                AddTag("units,", units).
-                AddField(fieldName, value).
-                SetTime(time.Now())
-        err := writeAPI.WritePoint(context.Background(), p)
-        handleNonFatalError("When saving integer datapoint with units", err)
-        influx_client.Close()
+func (ss *SimSession) Initialize(configPath string) error {
+	log.Println("Initializing SimSession...")
+	session_path, err := filepath.Abs(configPath)
+	if err != nil {
+		return fmt.Errorf("[Session Initialize]: failed to generate configPath: %w", err)
+	}
+	config, err := toml.LoadFile(session_path)
+	if err != nil {
+		return fmt.Errorf("[Session Initialize]: failed to load config file: %w", err)
+	}
+	err = ss.ParseValues(config)
+	if err != nil {
+		return fmt.Errorf("[Session Initialize]: failed to parse values: %w", err)
+	}
+	return nil
 }
 
-func (tsdb *TSDB) SaveFloatDataPointWithUnits(ss *SimSession, units string, fieldName string, value float64) {
-        influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
-        writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
-        p := influxdb2.NewPointWithMeasurement(ss.driver).
-                AddTag("location", ss.location).
-                AddTag("units,", units).
-                AddField(fieldName, value).
-                SetTime(time.Now())
-        err := writeAPI.WritePoint(context.Background(), p)
-        handleNonFatalError("When saving float datapoint with units", err)
-        influx_client.Close()
+func (tsdb *TSDB) SaveIntDataPoint(ss *SimSession, fieldName string, value int32) error {
+	influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
+	writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
+	q := influxdb2.NewPointWithMeasurement(ss.driver).
+		AddTag("location", ss.location).
+		AddField(fieldName, value).
+		SetTime(time.Now())
+	err := writeAPI.WritePoint(context.Background(), q)
+	if err != nil {
+		return fmt.Errorf("[TSDB SaveIntDataPoint]: %w", err)
+	}
+	influx_client.Close()
+	return nil
 }
 
-func (tsdb *TSDB) SaveFloatDataPoint(ss *SimSession, fieldName string, value float64) {
-        influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
-        writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
-        q := influxdb2.NewPointWithMeasurement(ss.driver).
-                AddTag("location", ss.location).
-                AddField(fieldName, value).
-                SetTime(time.Now())
-        err := writeAPI.WritePoint(context.Background(), q)
-        handleNonFatalError("When saving float datapoint with units", err)
-        influx_client.Close()
+func (tsdb *TSDB) SaveIntDataPointWithUnits(ss *SimSession, units string,
+	fieldName string, value int32) error {
+	influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
+	writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
+	p := influxdb2.NewPointWithMeasurement(ss.driver).
+		AddTag("location", ss.location).
+		AddTag("units,", units).
+		AddField(fieldName, value).
+		SetTime(time.Now())
+	err := writeAPI.WritePoint(context.Background(), p)
+	if err != nil {
+		return fmt.Errorf("[TSDB SaveIntDataPointWithUnits]: %w", err)
+	}
+	influx_client.Close()
+	return nil
 }
 
+func (tsdb *TSDB) SaveFloatDataPointWithUnits(ss *SimSession, units string, fieldName string, value float64) error {
+	influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
+	writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
+	p := influxdb2.NewPointWithMeasurement(ss.driver).
+		AddTag("location", ss.location).
+		AddTag("units,", units).
+		AddField(fieldName, value).
+		SetTime(time.Now())
+	err := writeAPI.WritePoint(context.Background(), p)
+	handleNonFatalError("When saving float datapoint with units", err)
+	if err != nil {
+		return fmt.Errorf("[TSDB SaveFloatDataPointWithUnits]: %w", err)
+	}
+	return nil
+}
+
+func (tsdb *TSDB) SaveFloatDataPoint(ss *SimSession, fieldName string, value float64) error {
+	influx_client := influxdb2.NewClient(tsdb.uri, tsdb.token)
+	writeAPI := influx_client.WriteAPIBlocking(tsdb.org, tsdb.bucket)
+	q := influxdb2.NewPointWithMeasurement(ss.driver).
+		AddTag("location", ss.location).
+		AddField(fieldName, value).
+		SetTime(time.Now())
+	err := writeAPI.WritePoint(context.Background(), q)
+	if err != nil {
+		return fmt.Errorf("[TSDB SaveFloatDataPoint]: %w", err)
+	}
+	influx_client.Close()
+	return nil
+}
--- pkg/tsdb/influxdb_test.go
+++ pkg/tsdb/influxdb_test.go
@@ -1,38 +1,175 @@
 package influxdb
 
 import (
+	"errors"
+	"strings"
 	"testing"
+
+	toml "github.com/pelletier/go-toml"
 )
 
-func TestTSDB(t *testing.T){
-    want := &TSDB{"localhost", 1234, "<some-token>", "http://localhost:1234", "some-org", "some-bucket"}
-    got := &TSDB{}
-    got.Initialize("conf_test.toml")
-    if got.host != want.host{
-        t.Errorf("got.host = %q, want.host = %q", got.host, want.host)
-    }
-    if got.port != want.port{
-        t.Errorf("got.port = %q, want.port = %q", got.port, want.port)
-    }
-    if got.uri != want.uri{
-        t.Errorf("got.uri = %q, want.uri = %q", got.uri, want.uri)
-    }
-    if got.token != want.token{
-        t.Errorf("got.token = %q, want.token = %q", got.token, want.token)
-    }
-    if got.org != want.org{
-        t.Errorf("got.org = %q, want.org = %q", got.org, want.org)
-    }
-    if got.bucket != want.bucket{
-        t.Errorf("got.bucket = %q, want.bucket = %q", got.bucket, want.bucket)
-    }
+func setupTestConfigurations() (map[int]*toml.Tree, map[int]*toml.Tree) {
+	connection_conf := make(map[int]*toml.Tree)
+	session_conf := make(map[int]*toml.Tree)
+	connection_conf[1], _ = toml.Load(`[influxdb_connection]`)
+	connection_conf[2], _ = toml.Load(`[influxdb_connection]
+host = "localhost"`)
+	connection_conf[3], _ = toml.Load(`[influxdb_connection]
+host = "localhost"
+port = 1234`)
+	connection_conf[4], _ = toml.Load(`[influxdb_connection]
+host = "localhost"
+port = 1234
+token = "<some-token>"`)
+	connection_conf[5], _ = toml.Load(`[influxdb_connection]
+host = "localhost"
+port = 1234
+token = "<some-token>"
+org = "some-org"
+`)
+	connection_conf[6], _ = toml.Load(`[influxdb_connection]
+host = "localhost"
+port = 1234
+token = "<some-token>"
+org = "some-org"
+bucket = "some-bucket"
+`)
+	session_conf[1], _ = toml.Load(`[session]`)
+	session_conf[2], _ = toml.Load(`[session]
+location = "some-location"
+`)
+	session_conf[3], _ = toml.Load(`[session]
+location = "some-location"
+driver = "some-driver"
+`)
+	session_conf[4], _ = toml.Load(`[session]
+location = "some-location"
+driver = "some-driver"
+gender = "some-gender"
+`)
+	return connection_conf, session_conf
 }
 
-func TestSimSession(t *testing.T){
-    want := &SimSession{"some-location", "some-driver", "some-gender"}
-    got := &SimSession{}
-    got.Initialize("conf_test.toml")
-    if got.location != want.location{
-        t.Errorf("got.location = %q, want.location = %q", got.location, want.location)
-    }
+func TestTSDMInitializationNonExistingFile(t *testing.T) {
+	want := errors.New("[TSDB Initialize]: failed to load config file:")
+	testTSDB := new(TSDB)
+	got := testTSDB.Initialize("non-existing-config")
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+}
+
+func TestTSDBParseValues(t *testing.T) {
+	tsdb := new(TSDB)
+	configurations, _ := setupTestConfigurations()
+	want := errors.New("[TSDB Initialize]: failed to parse one or various required values.")
+	for i := 1; i <= 5; i++ {
+		got := tsdb.ParseValues(configurations[i])
+		if !strings.Contains(got.Error(), want.Error()) {
+			t.Errorf("got = %q want = %q", got.Error(), want.Error())
+		}
+	}
+	got := tsdb.ParseValues(configurations[6])
+	if got != nil {
+		t.Errorf("Unexpected error when parsing config. %s", got.Error())
+	}
+}
+
+func TestTSDB(t *testing.T) {
+	want := &TSDB{"localhost", 1234, "<some-token>", "http://localhost:1234", "some-org", "some-bucket"}
+	got := &TSDB{}
+	got.Initialize("conf_test.toml")
+	if got.host != want.host {
+		t.Errorf("got.host = %q, want.host = %q", got.host, want.host)
+	}
+	if got.port != want.port {
+		t.Errorf("got.port = %q, want.port = %q", got.port, want.port)
+	}
+	if got.token != want.token {
+		t.Errorf("got.token = %q, want.token = %q", got.token, want.token)
+	}
+	if got.org != want.org {
+		t.Errorf("got.org = %q, want.org = %q", got.org, want.org)
+	}
+	if got.bucket != want.bucket {
+		t.Errorf("got.bucket = %q, want.bucket = %q", got.bucket, want.bucket)
+	}
+	if got.uri != want.uri {
+		t.Errorf("got.uri = %q, want.uri = %q", got.uri, want.uri)
+	}
+}
+
+func TestParseSessionValues(t *testing.T) {
+	test_session := new(SimSession)
+	_, configurations := setupTestConfigurations()
+	want := errors.New("[SimSession Initialize]: failed to parse one or various required values.")
+	for i := 1; i <= 3; i++ {
+		got := test_session.ParseValues(configurations[i])
+		if !strings.Contains(got.Error(), want.Error()) {
+			t.Errorf("got = %q want = %q", got.Error(), want.Error())
+		}
+	}
+	got := test_session.ParseValues(configurations[4])
+	if got != nil {
+		t.Errorf("Unexpected error when parsing config. %s", got.Error())
+	}
+}
+
+func TestSimSession(t *testing.T) {
+	want := &SimSession{"some-location", "some-driver", "some-gender"}
+	got := &SimSession{}
+	got.Initialize("conf_test.toml")
+	if got.location != want.location {
+		t.Errorf("got.location = %q, want.location = %q", got.location, want.location)
+	}
+	if got.driver != want.driver {
+		t.Errorf("got.driver = %q, want.driver = %q", got.driver, want.driver)
+	}
+	if got.gender != want.gender {
+		t.Errorf("got.gender = %q, want.gender = %q", got.gender, want.gender)
+	}
+}
+
+func TestSaveIntDataPoint(t *testing.T) {
+	testTSDB := &TSDB{"localhost", 1234, "<some-token>", "http://localhost:1234",
+		"some-org", "some-bucket"}
+	testSession := &SimSession{"some-location", "some-driver", "some-gender"}
+	want := errors.New("[TSDB SaveIntDataPoint]:")
+	got := testTSDB.SaveIntDataPoint(testSession, "some-field", 0)
+	if got == nil {
+		t.Errorf("Expected an error but didn't get one.")
+	}
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+	want = errors.New("[TSDB SaveIntDataPointWithUnits]:")
+	got = testTSDB.SaveIntDataPointWithUnits(testSession, "some-units", "some-field", 0)
+	if got == nil {
+		t.Errorf("Expected an error but didn't get one.")
+	}
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+}
+
+func TestSaveFloatDataPoint(t *testing.T) {
+	testTSDB := &TSDB{"localhost", 1234, "<some-token>", "http://localhost:1234",
+		"some-org", "some-bucket"}
+	testSession := &SimSession{"some-location", "some-driver", "some-gender"}
+	want := errors.New("[TSDB SaveFloatDataPoint]:")
+	got := testTSDB.SaveFloatDataPoint(testSession, "some-field", 0)
+	if got == nil {
+		t.Errorf("Expected an error but didn't get one.")
+	}
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+	want = errors.New("[TSDB SaveFloatDataPointWithUnits]:")
+	got = testTSDB.SaveFloatDataPointWithUnits(testSession, "some-units", "some-field", 0)
+	if got == nil {
+		t.Errorf("Expected an error but didn't get one.")
+	}
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
 }
