e49e230fc032f5bc62ae7f2b63f0a89a4f4dfad6
David Claudio Gonzalez <d.claudiogonzalez@gmail.com>
2023-05-09 04:13:01 -0500

    Refactor VideoRecorder package
    Signed-off-by: David Claudio Gonzalez <d.claudiogonzalez@gmail.com>

---
+++ pkg/videorecorder/obs_client.go
@@ -0,0 +1,93 @@
+package obs_client
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"path/filepath"
+	"strconv"
+
+	"github.com/pelletier/go-toml"
+	"github.com/stretchr/testify/mock"
+)
+
+type VideoRecorder struct {
+	obs_host       string
+	obs_port       int64
+	obs_pwd        string
+	obs_connection string
+}
+
+type publicOBS interface {
+	GetVersion()
+	StartRecording()
+	StopRecording()
+}
+
+type mockVideoRecorder struct{ mock.Mock }
+
+func newMockVideoRecorder() *mockVideoRecorder {
+	return &mockVideoRecorder{}
+}
+
+func (vr *VideoRecorder) ParseValues(config *toml.Tree) (err error) {
+	defer func() error {
+		if recover() != nil {
+			err = errors.New("[VideoRecorder Initialize]: failed to parse one or various required values.")
+		}
+		return err
+	}()
+	vr.obs_host = config.Get("obs_connection.host").(string)
+	vr.obs_port = config.Get("obs_connection.port").(int64)
+	vr.obs_pwd = config.Get("obs_connection.password").(string)
+	return nil
+}
+
+func (vr *VideoRecorder) Initialize(configPath string) error {
+	log.Println("Initializing VideoRecorder...")
+	config_path, err := filepath.Abs(configPath)
+	if err != nil {
+		return fmt.Errorf("[VideoRecorder Initialize]: failed to generate configPath: %w", err)
+	}
+	config, err := toml.LoadFile(config_path)
+	if err != nil {
+		return fmt.Errorf("[VideoRecorder Initialize]: failed to load config file: %w", err)
+	}
+	err = vr.ParseValues(config)
+	if err != nil {
+		return fmt.Errorf("[VideoRecorder Initialize]: failed to parse values: %w", err)
+	}
+	vr.obs_connection = fmt.Sprintf("%s:%s", vr.obs_host, strconv.Itoa(int(vr.obs_port)))
+	log.Println("Will connect to the following Websocket Server:", vr.obs_connection)
+	return nil
+}
+
+func (mockVR *mockVideoRecorder) GetVersion() error {
+	args := mockVR.Called()
+	return args.Error(0)
+}
+
+func (vr *VideoRecorder) GetVersion() error {
+	log.Println("[VideoRecorder GetVersion]: Not yet implemented")
+	return nil
+}
+
+func (mockVR *mockVideoRecorder) StartRecording() error {
+	args := mockVR.Called()
+	return args.Error(0)
+}
+
+func (vr *VideoRecorder) StartRecording() error {
+	log.Println("[VideoRecorder GetVersion]: Not yet implemented")
+	return nil
+}
+
+func (mockVR *mockVideoRecorder) StopRecording() error {
+	args := mockVR.Called()
+	return args.Error(0)
+}
+
+func (vr *VideoRecorder) StopRecording() error {
+	log.Println("[VideoRecorder GetVersion]: Not yet implemented")
+	return nil
+}
---
+++ pkg/videorecorder/obs_client_test.go
@@ -0,0 +1,113 @@
+package obs_client
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	"github.com/andreykaipov/goobs"
+	toml "github.com/pelletier/go-toml"
+)
+
+type MockGoobs struct {
+	client *goobs.Client
+}
+
+type MockGeneral struct {
+	subclients mockSubclients
+}
+
+type mockSubclients struct {
+	General *goobs.Client
+}
+
+func setupTestConfigurations() map[int]*toml.Tree {
+	obs_conf := make(map[int]*toml.Tree)
+	obs_conf[1], _ = toml.Load(`[obs_connection]`)
+	obs_conf[2], _ = toml.Load(`[obs_connection]
+host = "some-host"
+`)
+	obs_conf[3], _ = toml.Load(`[obs_connection]
+host = "some-host"
+port = 1234
+`)
+	obs_conf[4], _ = toml.Load(`[obs_connection]
+    host = "some-host"
+    port = 1234
+    password = "some-password"
+`)
+	return obs_conf
+}
+
+func Test_VideoRecorder_InitializationNonExistingFile(t *testing.T) {
+	want := errors.New("[VideoRecorder Initialize]: failed to load config file:")
+	testVR := new(VideoRecorder)
+	got := testVR.Initialize("non-existing-config")
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+}
+
+func Test_VideoRecorder_Initialize(t *testing.T) {
+	want := &VideoRecorder{"some-host", 1234, "some-password", "some-connection"}
+	got := &VideoRecorder{}
+	got.Initialize("conf_test.toml")
+	if got.obs_host != want.obs_host {
+		t.Errorf("got.obs_host = %q, want.obs_host = %q", got.obs_host, want.obs_host)
+	}
+	if got.obs_port != want.obs_port {
+		t.Errorf("got.obs_port = %q, want.obs_port = %q", got.obs_port, want.obs_port)
+	}
+	if got.obs_pwd != want.obs_pwd {
+		t.Errorf("got.obs_pwd = %q, want.obs_pwd = %q", got.obs_pwd, want.obs_pwd)
+	}
+}
+
+func Test_VideoRecorder_ParseValues(t *testing.T) {
+	testVR := new(VideoRecorder)
+	configurations := setupTestConfigurations()
+	want := errors.New("[VideoRecorder Initialize]: failed to parse one or various required values.")
+	for i := 1; i <= 3; i++ {
+		got := testVR.ParseValues(configurations[i])
+		if !strings.Contains(got.Error(), want.Error()) {
+			t.Errorf("got = %q want = %q", got.Error(), want.Error())
+		}
+	}
+	got := testVR.ParseValues(configurations[4])
+	if got != nil {
+		t.Errorf("Unexpected error when parsing config. %s", got.Error())
+	}
+}
+
+func Test_GetVersion(t *testing.T) {
+	vr := new(VideoRecorder)
+	vr.Initialize("conf_test.toml")
+	mockVR := newMockVideoRecorder()
+	mockVR.On("GetVersion").Return(nil)
+	got := mockVR.GetVersion()
+	if got != nil {
+		t.Errorf("Unexpected error when parsing calling GetVersion")
+	}
+}
+
+func Test_StartRecording(t *testing.T) {
+	vr := new(VideoRecorder)
+	vr.Initialize("conf_test.toml")
+	mockVR := newMockVideoRecorder()
+	mockVR.On("StartRecording").Return(nil)
+	got := mockVR.StartRecording()
+	if got != nil {
+		t.Errorf("Unexpected error when parsing calling StartRecording")
+	}
+}
+
+func Test_StopRecording(t *testing.T) {
+	vr := new(VideoRecorder)
+	vr.Initialize("conf_test.toml")
+	mockVR := newMockVideoRecorder()
+	mockVR.On("StartRecording").Return(nil)
+	got := mockVR.StartRecording()
+	if got != nil {
+		t.Errorf("Unexpected error when parsing calling StartRecording")
+	}
+}
