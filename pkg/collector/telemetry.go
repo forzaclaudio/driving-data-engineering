0648c69d98d0328cfbff1e070e6d8eb134e50f3c
David Claudio Gonzalez <d.claudiogonzalez@gmail.com>
2023-05-11 05:22:03 -0500

    Add first version of telemetry collector package
    Signed-off-by: David Claudio Gonzalez <d.claudiogonzalez@gmail.com>

---
+++ pkg/collector/telemetry.go
@@ -0,0 +1,50 @@
+package f1_telemetry
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"path/filepath"
+
+	"github.com/pelletier/go-toml"
+)
+
+type TelemetryClient struct {
+	udp_port int64
+}
+
+func (tc *TelemetryClient) ParseValues(config *toml.Tree) (err error) {
+	defer func() error {
+		if recover() != nil {
+			err = errors.New("[Telemetry Client Initialize]: failed to parse one or various required values.")
+		}
+		return err
+	}()
+	tc.udp_port = config.Get("telemetry_client.udp_port").(int64)
+	log.Println("Client will listen on:", tc.udp_port)
+	return nil
+}
+
+func (tc *TelemetryClient) Initialize(configPath string) error {
+	log.Println("Initializing Telemetry Client...")
+	config_path, err := filepath.Abs(configPath)
+	if err != nil {
+		return fmt.Errorf("[Telemetry Client Initialize]: failed to generate configPath: %w", err)
+	}
+	config, err := toml.LoadFile(config_path)
+	if err != nil {
+		return fmt.Errorf("[Telemetry Client Initialize]: failed to load config file: %w", err)
+	}
+	err = tc.ParseValues(config)
+	if err != nil {
+		return fmt.Errorf("[Telemetry Client Initialize]: failed to parse values: %w", err)
+	}
+	log.Println("Will listen on", tc.udp_port)
+	return nil
+}
+
+/*client, err := telemetry.NewClientByCustomIpAddressAndPort("0.0.0.0", 20777)
+	if err != nil {
+		log.Fatal("When creating telemetry client:", err)
+	}
+fmt.Println(client)*/
---
+++ pkg/collector/telemetry_test.go
@@ -0,0 +1,49 @@
+package f1_telemetry
+
+import (
+	"errors"
+	"strings"
+	"testing"
+
+	toml "github.com/pelletier/go-toml"
+)
+
+func setupTestConfigurations() map[int]*toml.Tree {
+	telemetry_conf := make(map[int]*toml.Tree)
+	telemetry_conf[1], _ = toml.Load(`[telemetry_client]`)
+	telemetry_conf[2], _ = toml.Load(`[telemetry_client]
+udp_port = 20777`)
+	return telemetry_conf
+}
+
+func Test_TelemetryClient_Initialization_NonExistingFile(t *testing.T) {
+	want := errors.New("[Telemetry Client Initialize]: failed to load config file:")
+	tc := new(TelemetryClient)
+	got := tc.Initialize("non-existing-config")
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+}
+
+func Test_TelemetryClient_ParseValues(t *testing.T) {
+	tc := new(TelemetryClient)
+	configurations := setupTestConfigurations()
+	want := errors.New("[Telemetry Client Initialize]: failed to parse one or various required values.")
+	got := tc.ParseValues(configurations[1])
+	if !strings.Contains(got.Error(), want.Error()) {
+		t.Errorf("got = %q want = %q", got.Error(), want.Error())
+	}
+	got = tc.ParseValues(configurations[2])
+	if got != nil {
+		t.Errorf("Unexpected error when parsing config. %s", got.Error())
+	}
+}
+
+func Test_TelemetryClient(t *testing.T) {
+	want := &TelemetryClient{20777}
+	got := &TelemetryClient{}
+	got.Initialize("conf_test.toml")
+	if got.udp_port != want.udp_port {
+		t.Errorf("got.udp_port = %q, want.upd_port = %q", got.udp_port, want.udp_port)
+	}
+}
